51858c779cfc5a9350a0d2ee7f73e2d22529cf63
walisonfbalves <walisonfbalves.ti@gmail.com>
2024-09-19 00:23:50 -0300

    Concluindo a parte do template

--- clientes-app/src/app/template/navbar/navbar.component.ts
+++ clientes-app/src/app/template/navbar/navbar.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, } from '@angular/core';
+
+import jQuery from 'jquery';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.toggleSideBar()
+  }
+
+  toggleSideBar() {
+    (function ($) {
+      "use strict";
+
+      var path = window.location.href;
+      $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function () {
+        if (this.href === path) {
+          $(this).addClass("active");
+        }
+      });
+
+      $("#sidebarToggle").on("click", function (e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+      });
+    })(jQuery);
   }
 
 }

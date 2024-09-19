9d9f9be3dfe41d9894b1913d8a250a67d8f1e014
walisonfbalves <walisonfbalves.ti@gmail.com>
2024-09-19 16:01:17 -0300

    Desenvolvendo o formulário de clientes

--- clientes-app/src/app/clientes/clientes-routing.module.ts
+++ clientes-app/src/app/clientes/clientes-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClientesFormComponent } from './clientes-form/clientes-form.component';
 
-
 const routes: Routes = [
-  {path: 'clientes-form', component: ClientesFormComponent}
+  { path: 'clientes-form', component: ClientesFormComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class ClientesRoutingModule { }
